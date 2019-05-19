0a78d5a6c38648c6977ba129fd439de1b59f2efa
neilobrien <nobrien@gmail.com>
2019-05-19 15:05:08 +0100

    Test Commit

---
+++ server/server.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func d(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "dog dog dog")
+}
+
+func c(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "cat cat cat")
+}
+
+func main() {
+
+	http.HandleFunc("/dog", d)
+	http.HandleFunc("/cat", c)
+
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("Hello World")
+}
